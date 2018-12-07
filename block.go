1cbf12653ff8e15e60364b5e3116ccaa90a4bd71
dkcyber <383962714@qq.com>
2018-12-07 16:18:17 +0800

    初始化Block结构体

---
+++ block.go
@@ -0,0 +1,15 @@
+package main
+
+//区块结构体
+type Block struct {
+	Version uint64  //版本号
+	PreBlockHash []byte //前一个区块的哈希
+	MerkleRoot []byte //梅克尔根
+	TimpStamp uint64 //区块创建时间
+	Difficulty uint64 //挖矿难度值
+	Nonce uint64 //随机数
+	Data []byte //区块交易数据
+	Hash []byte //本区块哈希，为了操作方便，实际不含此项
+}
+
+//创建区块
